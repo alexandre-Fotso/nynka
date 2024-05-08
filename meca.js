7c4c036776f10a07a0f148aff62d3b19c88f9c1c
Alexandre Fotso <123139827+alexandre-Fotso@users.noreply.github.com>
2024-05-08 09:18:40 +0100

    Add files via upload

---
+++ meca.js
@@ -0,0 +1,20 @@
+function comboInit(thelist)
+{
+  theinput = document.getElementById(theinput);  
+  var idx = thelist.selectedIndex;
+  var content = thelist.options[idx].innerHTML;
+  if(theinput.value == "")
+    theinput.value = content;	
+}
+
+function combo(thelist, theinput)
+{
+  theinput = document.getElementById(theinput);  
+  var idx = thelist.selectedIndex;
+  var content = thelist.options[idx].innerHTML;
+  theinput.value = content;	
+}
+
+
+
+//var content = thelist.options[idx].value;  
